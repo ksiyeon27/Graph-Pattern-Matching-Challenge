8d206218236c85284ac9ac6fc5bde50aaf888b2b
Myeongseop Jeon <jeonms1611@gmail.com>
2021-05-23 18:56:05 +0900

    Fix checker not compiling

--- checker/checker.cc
+++ checker/checker.cc
@@ -1,25 +1,31 @@
 /**
- * @file checker.cc
+ * @file main.cc
  *
  */
 
+#include "backtrack.h"
+#include "candidate_set.h"
 #include "common.h"
 #include "graph.h"
 
-void checker(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   if (argc < 4) {
     std::cerr << "Usage: ./program <data graph file> <query graph file> "
-                 "<output file>\n";
+                 "<candidate set file>\n";
+    return EXIT_FAILURE;
   }
 
   std::string data_file_name = argv[1];
   std::string query_file_name = argv[2];
-  std::string output_file_name = argv[3];
+  std::string candidate_set_file_name = argv[3];
 
   Graph data(data_file_name);
   Graph query(query_file_name, true);
+  CandidateSet candidate_set(candidate_set_file_name);
 
-  size_t vnum;
-  vnum = query.GetNumVertices();
-  std::cout << vnum;
+  Backtrack backtrack;
+
+  backtrack.PrintAllMatches(data, query, candidate_set);
+
+  return EXIT_SUCCESS;
 }
