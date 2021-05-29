e63ca4a0a7b634b688c7c09a1830be362b6ff84f
Myeongseop Jeon <jeonms1611@gmail.com>
2021-05-29 14:58:07 +0900

    Add debugging functions

--- include/common.h
+++ include/common.h
@@ -19,4 +19,30 @@
 using Vertex = int32_t;
 using Label = int32_t;
 
+#ifndef NDEBUG
+#define GRAPH_PATTERN_MATCHING_CHALLENGE_LOG(...)                   \
+  {                                                                 \
+    fprintf(stderr, "%s:%d: %s(): ", __FILE__, __LINE__, __func__); \
+    fprintf(stderr, __VA_ARGS__);                                   \
+    fprintf(stderr, "\n");                                          \
+  }
+#else
+#define GRAPH_PATTERN_MATCHING_CHALLENGE_LOG(...)
+#endif
+
+template <typename T>
+std::string vector_to_string(const std::vector<T> &v) {
+  if (v.empty()) {
+    return "[]";
+  } else {
+    std::string ret = "[";
+    ret += std::to_string(v.front());
+    for (size_t i = 1; i < v.size(); ++i) {
+      ret = ret + ", " + std::to_string(v[i]);
+    }
+    ret += "]";
+    return ret;
+  }
+}
+
 #endif  // COMMON_H_
